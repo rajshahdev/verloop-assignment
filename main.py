677b2b1d0f015a640ad190279c0091eb99296c19
rajshahdev <be.rajshah@gmail.com>
2022-01-30 11:28:12 +0530

    adding env-example and other files

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
-from msilib import schema
-from fastapi import FastAPI
-from . import schemas
-from .config import Settings
+from fastapi import FastAPI, status, HTTPException, Response
+from schemas import GetAddress
+from config import settings
 import googlemaps
 
 
-gmaps = googlemaps.Client(key=Settings.api)
+gmaps = googlemaps.Client(key=settings.api)
 app = FastAPI()
 
-@app.post('getaddress')
-def getaddress(add:schemas.GetAddress):
+@app.post('/getaddress', status_code=status.HTTP_200_OK)
+def getaddress(add:GetAddress):
+    if not add.address and not add.output_format:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"enter a valid address and output type")
+    if not add.address:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"enter a valid address")
+    if not add.output_format:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"enter a valid output type")
+
+    latlng = gmaps.geocode(add.address)[0]['geometry']['viewport']['southwest']
+    lat = latlng['lat']
+    lng = latlng['lng']
+    if add.output_format == "xml":
+        data = f"""
+        <?xml version="1.0" encoding="UTF-8"?>
+            <root>
+                <address>{add.address}</address>
+                <coordinates>
+                    <lat>{lat}</lat>
+                    <lng>{lng}</lng>
+            </coordinates>
+        </root>
+        """
+        return Response(content=data, media_type="application/xml")
+    elif add.output_format == "json":
+        return {"coordinates":{"lat":lat,"lng":lng}, "address":add.address}
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"enter a valid output type")    
     
-    return gmaps.geocode(add.address)[0]['geometry']['viewport']['southwest']
--- schemas.py
+++ schemas.py
@@ -2,4 +2,4 @@ from pydantic import BaseModel
 
 class GetAddress(BaseModel):
     address:str
-    # output_format:str
+    output_format:str
